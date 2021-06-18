47ccb167ae0421a5a9f69de1a60b6e8fb2375fe7
Clément Dauvilliers <clem.dauvilliers@gmail.com>
2021-06-18 16:52:05 +0200

    Added the discriminator The segmentation GAN is now functional

--- losses.py
+++ losses.py
@@ -34,32 +34,33 @@ def weighted_categorical_crossentropy(weights):
         # Applies the weights
         loss = loss * weights
 
-        return loss
+        return tf.reduce_mean(loss)
 
     return loss
 
 
-def generator_loss(disc_generated_output, gen_output, target):
+def generator_loss(disc_generated_output, gen_output, target, base_loss_func):
     """
     Loss for the generator network.
     -- disc_generated_output: Discriminator output on the generated images.
                               Should be given as logits, not probabilities.
     -- gen_output: Generator output (Discriminator's input)
     -- target: groundtruth data
+    -- base_loss_func: Function to use as base loss (crossentropy,
+                       L1 distance, ...).
     returns: T, G, bc where T is the total loss, g is the gan loss term,
     and bc is the binary crossentropy term.
     """
     # We compare the disc's output on fake images to an array of ones
     # because the generator wants the disc to output ones on fake images
-    # gan_loss = bin_crossentropy(tf.ones_like(disc_generated_output),
-    #                             disc_generated_output)
-    # base_loss = weighted_loss(gen_output, target)
-    # base_loss = weighted_loss(target, gen_output)
+    gan_loss = bin_crossentropy(tf.ones_like(disc_generated_output),
+                                disc_generated_output)
+    base_loss = base_loss_func(target, gen_output) / 15
 
     # the final loss mixes both terms, with the default lambda coeff of 10
-    total_gen_loss = None
+    total_gen_loss = base_loss + gan_loss
 
-    return total_gen_loss, None, None
+    return total_gen_loss, gan_loss, base_loss
 
 
 def discriminator_loss(disc_real_output, disc_generated_output):
--- segmentationGAN.py
+++ segmentationGAN.py
@@ -5,9 +5,11 @@ Defines the SegmentationGAN class
 import os
 import tensorflow as tf
 import numpy as np
+import time
 import segmentationGAN.losses as losses
 from skimage.io import imsave
 from segmentationGAN.image import convert_to_8bits_rgb
+from segmentationGAN.models import get_patch_discriminator
 
 
 class SegmentationGAN:
@@ -15,19 +17,25 @@ class SegmentationGAN:
     A Segmentation GAN is a model adapted from
     P.Isola et al. )s pix2pix for segmentation tasks.
     """
-    def __init__(self, name, generator, nb_classes):
+    def __init__(self, name, generator, input_shape, nb_classes):
         """
         -- name: Name of this model
         -- generator: tf.keras.Model to be used as generator
         -- nb_classes: Number of classes in this segmentation task.
         """
         self._gen = generator
+        self._disc = get_patch_discriminator(input_shape, nb_classes)
+
         self._name = name
         self._nb_classes = nb_classes
+        self._gen_optim = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+        self._disc_optim = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
     def train(self,
               inputs,
               targets,
+              epochs=1,
+              batch_size=32,
               validation_data=None,
               checkpoint_dir="./",
               class_weights=None):
@@ -38,6 +46,9 @@ class SegmentationGAN:
                     The target must be encoded sparsely, i.e.
                     target[i, j, k] = c where c is the index of one
                     of the segmentation classes.
+        -- epochs: Number of epochs of training
+        -- batch_size: Well, that's the batch size. The higher the batch size,
+                       the more GPU memory-consuming.
         -- validation_data: optional couple (valid_inputs, valid_targets)
                             to evaluate the network after training.
         -- class_weights: Array indicating the weight for each segmentation
@@ -57,14 +68,78 @@ class SegmentationGAN:
                              self._nb_classes,
                              axis=1)
 
-        self._gen.compile(optimizer="adam", loss=weighted_loss)
-        self._gen.fit(inputs, targets, epochs=5, batch_size=32)
+        # TRAINING LOOP ============================================
+
+        # Split the data into batches
+        total_batches = int(np.ceil(inputs.shape[0] // batch_size))
+        input_batches = np.array_split(inputs, total_batches)
+        target_batches = np.array_split(targets, total_batches)
+
+        for n_epoch in range(epochs):
+            epoch_start_time = time.time()
+            for n_batch, (inpt, target) in enumerate(
+                    zip(input_batches, target_batches)):
+
+                gen_loss, gan_loss, base_loss, disc_loss = self.train_step(
+                    inpt, target, weighted_loss)
+                print("Batch {} /{}      ".format(n_batch, total_batches),
+                      end="\r")
+
+            print("Epoch {} ended in {:1.3f}s".format(
+                n_epoch + 1,
+                time.time() - epoch_start_time))
+            print(
+                "GEN base loss: {:1.3f} gan loss: {:1.3f} total loss: {:1.3f} \
+DISC loss: {:1.3f}".format(base_loss, gan_loss, gen_loss, disc_loss))
+
+        # Save the model at the end of each epoch
+        print("Saving model...")
         self._gen.save(
             os.path.join(checkpoint_dir, self._name + "_best_weights.h5"))
 
+        # ===========================================================
+
+        # Applies the gen to the validation data
+        print("Applying to the validation data")
         if validation_data is not None:
             valid_inpt, valid_target = validation_data
             self.save_validation_examples(valid_inpt, valid_target)
+
+    @tf.function
+    def train_step(self, input_image, target, loss):
+        """
+        Performs a single training step over a batch of data.
+        -- input_image: Input data
+        -- target: Target data
+        -- loss: Loss function to use as base loss (could be crossentropy,
+                 L1 distance, etc..).
+        """
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            # Forward pass
+            gen_output = self._gen(input_image, training=True)
+            disc_real_output = self._disc([input_image, target], training=True)
+            disc_generated_output = self._disc([input_image, gen_output],
+                                               training=True)
+
+            # Compute the losses
+            gen_loss, gen_gan_loss, gen_base_loss = losses.generator_loss(
+                disc_generated_output, gen_output, target, loss)
+            disc_loss = losses.discriminator_loss(disc_real_output,
+                                                  disc_generated_output)
+
+            # Compute the gradients
+            gen_gradients = gen_tape.gradient(gen_loss,
+                                              self._gen.trainable_variables)
+            disc_gradients = disc_tape.gradient(disc_loss,
+                                                self._disc.trainable_variables)
+
+            # Optimize the models' parameters
+            self._gen_optim.apply_gradients(
+                zip(gen_gradients, self._gen.trainable_variables))
+            self._disc_optim.apply_gradients(
+                zip(disc_gradients, self._disc.trainable_variables))
+
+        return gen_loss, gen_gan_loss, gen_base_loss, disc_loss
 
     def save_validation_examples(self,
                                  inputs,
