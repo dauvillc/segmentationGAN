180d452a046ab33fa10ecb8a423d60e968c6a585
Clément Dauvilliers <clem.dauvilliers@gmail.com>
2021-06-23 14:13:29 +0200

    Save every 20 epochs

--- segmentationGAN.py
+++ segmentationGAN.py
@@ -10,6 +10,7 @@ import segmentationGAN.losses as losses
 from skimage.io import imsave
 from segmentationGAN.image import convert_to_8bits_rgb
 from segmentationGAN.models import get_patch_discriminator
+from segmentationGAN.preprocessing import coshuffle_arrays, coshuffle_lists
 
 
 class SegmentationGAN:
@@ -28,8 +29,6 @@ class SegmentationGAN:
 
         self._name = name
         self._nb_classes = nb_classes
-        self._gen_optim = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-        self._disc_optim = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 
     def train(self,
               inputs,
@@ -63,25 +62,48 @@ class SegmentationGAN:
             weighted_loss = losses.weighted_categorical_crossentropy(
                 np.ones((self._nb_classes, )))
 
+        # Shuffles the data before starting the training
+        inputs, targets = coshuffle_arrays(inputs, targets)
+
         # First, the targets are converted to one-hot encoding
         targets = tf.one_hot(targets.astype(np.uint8),
                              self._nb_classes,
                              axis=1)
-
-        # TRAINING LOOP ============================================
 
         # Split the data into batches
         total_batches = int(np.ceil(inputs.shape[0] // batch_size))
         input_batches = np.array_split(inputs, total_batches)
         target_batches = np.array_split(targets, total_batches)
 
+        # Optimizers and Hyperparameters
+        gen_lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            0.001, decay_steps=epochs * batch_size, decay_rate=0.9)
+
+        disc_lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            0.001, decay_steps=epochs * batch_size, decay_rate=0.9)
+        gen_optim = tf.keras.optimizers.Adam(learning_rate=gen_lr_schedule,
+                                             beta_1=0.5)
+        disc_optim = tf.keras.optimizers.Adam(learning_rate=disc_lr_schedule,
+                                              beta_1=0.5)
+
+        # Make sure the checkpoints directory actually exists
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        savefile = os.path.join(checkpoint_dir, self._name + "_best_weights.h5")
+
+        # TRAINING LOOP ============================================
+
         for n_epoch in range(epochs):
             epoch_start_time = time.time()
+
+            input_batches, target_batches = coshuffle_lists(
+                input_batches, target_batches)
+
             for n_batch, (inpt, target) in enumerate(
                     zip(input_batches, target_batches)):
 
                 gen_loss, gan_loss, base_loss, disc_loss = self.train_step(
-                    inpt, target, weighted_loss)
+                    inpt, target, weighted_loss, gen_optim, disc_optim)
                 print("Batch {} /{}      ".format(n_batch, total_batches),
                       end="\r")
 
@@ -91,11 +113,13 @@ class SegmentationGAN:
             print(
                 "GEN base loss: {:1.3f} gan loss: {:1.3f} total loss: {:1.3f} \
 DISC loss: {:1.3f}".format(base_loss, gan_loss, gen_loss, disc_loss))
+            print("Current LR: ",
+                  gen_optim.learning_rate((n_epoch + 1) * batch_size))
 
-        # Save the model at the end of each epoch
-        print("Saving model...")
-        self._gen.save(
-            os.path.join(checkpoint_dir, self._name + "_best_weights.h5"))
+            # Save the model every 20 epochs
+            if n_epoch % 20 == 0:
+                print("Saving model into ", savefile)
+                self._gen.save(savefile)
 
         # ===========================================================
 
@@ -106,13 +130,15 @@ DISC loss: {:1.3f}".format(base_loss, gan_loss, gen_loss, disc_loss))
             self.save_validation_examples(valid_inpt, valid_target)
 
     @tf.function
-    def train_step(self, input_image, target, loss):
+    def train_step(self, input_image, target, loss, gen_optim, disc_optim):
         """
         Performs a single training step over a batch of data.
         -- input_image: Input data
         -- target: Target data
         -- loss: Loss function to use as base loss (could be crossentropy,
                  L1 distance, etc..).
+        -- gen_optim: Optimizer object for the generator
+        -- disc_optim: Optimizer object for the discriminator
         """
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
             # Forward pass
@@ -134,9 +160,9 @@ DISC loss: {:1.3f}".format(base_loss, gan_loss, gen_loss, disc_loss))
                                                 self._disc.trainable_variables)
 
             # Optimize the models' parameters
-            self._gen_optim.apply_gradients(
+            gen_optim.apply_gradients(
                 zip(gen_gradients, self._gen.trainable_variables))
-            self._disc_optim.apply_gradients(
+            disc_optim.apply_gradients(
                 zip(disc_gradients, self._disc.trainable_variables))
 
         return gen_loss, gen_gan_loss, gen_base_loss, disc_loss
@@ -167,3 +193,12 @@ DISC loss: {:1.3f}".format(base_loss, gan_loss, gen_loss, disc_loss))
                    seg,
                    check_contrast=False)
         return
+
+    def segment(self, x, results_dir="seggan_test", batch_size=1):
+        """
+        Performs segmentation on a given set of data.
+        -- x: Input data, as an array of shape (batch_size, c, h, w).
+        returns the segmentations as an array of shape (batch_size, h, w)
+        """
+        probas = self.__gen.predict(x, batch_size=batch_size)
+        return np.argmax(probas, axis=1)
