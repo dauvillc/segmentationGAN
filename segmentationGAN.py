4340d7bf750c002a08648f9337da6352a3ace5f4
Clément Dauvilliers <clem.dauvilliers@gmail.com>
2021-06-29 13:44:01 +0200

    One more try to fix the printing of the LR schedule.

--- segmentationGAN.py
+++ segmentationGAN.py
@@ -115,7 +115,7 @@ class SegmentationGAN:
             print(
                 "GEN base loss: {:1.3f} gan loss: {:1.3f} total loss: {:1.3f} \
 DISC loss: {:1.3f}".format(base_loss, gan_loss, gen_loss, disc_loss))
-            print("Current LR: ", gen_lr_schedule)
+            print("Current LR: ", gen_optim.learning_rate)
 
             # Save the model every 20 epochs
             if n_epoch % 20 == 0:
